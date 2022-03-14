dc15ddab1214d8e2a0bfe376ba4d5a94bcce1ad1
Marco Mascorro <m@mascobot.com>
2022-03-14 13:56:38 -0700

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,16 @@
+from wand.image import Image
+import os
+
+SourceFolder="/"
+TargetFolder="/
+
+for file in os.listdir(SourceFolder):
+    if 'HEIC' in file:
+        SourceFile=SourceFolder + "/" + file
+        TargetFile=TargetFolder + "/" + file.replace(".HEIC",".JPG")
+
+        img=Image(filename=SourceFile)
+        img.format='jpg'
+        img.save(filename=TargetFile)
+        img.close()
+print ('finished')
